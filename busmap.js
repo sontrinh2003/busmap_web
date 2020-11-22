411e0b1dee94f5ba00b8767b1524157f74475358
sontrinh2003 <69790959+sontrinh2003@users.noreply.github.com>
2020-11-22 10:36:19 +0700

    Update busmap.js

--- busmap.js
+++ busmap.js
@@ -1,83 +1,87 @@
-let audio = [];
+function allFunction()
+{
+    let audio = [];
 
-function createaudio(url) {
-    audio.push(new Audio(url));
-}
-
-for (let i = 0; i <= urls.length; i++) createaudio(urls[i]);
-
-let playbut = document.querySelectorAll('.playbutton')
-
-function playaudio(i) {
-    if (!audio[i].paused)
-    {
-        audio[i].pause();
-        audio[i].currentTime = 0;
-        playbut[i].src = "play_button.jpg";
-        return;
+    function createaudio(url) {
+        audio.push(new Audio(url));
     }
-    
-    for (let j = 0; j < audio.length; j++)
-    {
-        if (!audio[j].paused)
+
+    for (let i = 0; i <= urls.length; i++) createaudio(urls[i]);
+
+    let playbut = document.querySelectorAll('.playbutton')
+
+    function playaudio(i) {
+        if (!audio[i].paused)
         {
-            audio[j].pause();
-            audio[j].currentTime = 0;
-            playbut[j].src = "play_button.jpg";
+            audio[i].pause();
+            audio[i].currentTime = 0;
+            playbut[i].src = "play_button.jpg";
+            return;
         }
-        else
+
+        for (let j = 0; j < audio.length; j++)
         {
-            audio[i].play();
-            playbut[i].src = "stop_button.jpg";
-            audio[i].addEventListener('ended', function() {
-                playbut[i].src = "play_button.jpg"
-            });
+            if (!audio[j].paused)
+            {
+                audio[j].pause();
+                audio[j].currentTime = 0;
+                playbut[j].src = "play_button.jpg";
+            }
+            else
+            {
+                audio[i].play();
+                playbut[i].src = "stop_button.jpg";
+                audio[i].addEventListener('ended', function() {
+                    playbut[i].src = "play_button.jpg"
+                });
+            }
         }
     }
+
+    let but1 = document.querySelector('#departbutt');
+    let but2 = document.querySelector('#returnbutt');
+    let di = document.querySelectorAll('.depart');
+    let ve = document.querySelectorAll('.return');
+
+    function selectdepart() {
+        but1.style.backgroundColor = "darkgray";
+        but2.style.backgroundColor = "lightgray";
+
+        for (let i = 0; i < di.length; i++)
+        {
+            di[i].style.display = "block";
+        }
+
+        for (let i = 0; i < ve.length; i++)
+        {
+            ve[i].style.display = "none";
+        }
+
+        for (let i = 0; i <= urls.length; i++) {
+            audio[i].pause();
+            audio[i].currentTime = 0;
+        }
+    }
+
+    function selectreturn() {
+        but1.style.backgroundColor = "lightgray";
+        but2.style.backgroundColor = "darkgray";
+
+        for (let i = 0; i < di.length; i++)
+        {
+            di[i].style.display = "none";
+        }
+
+        for (let i = 0; i < ve.length; i++)
+        {
+            ve[i].style.display = "block";
+        }
+
+        for (let i = 0; i <= urls.length; i++) {
+            audio[i].pause();
+            audio[i].currentTime = 0;
+        }        
+    }
 }
 
-
-let but1 = document.querySelector('#departbutt');
-let but2 = document.querySelector('#returnbutt');
-let di = document.querySelectorAll('.depart');
-let ve = document.querySelectorAll('.return');
-
-function selectdepart() {
-    but1.style.backgroundColor = "darkgray";
-    but2.style.backgroundColor = "lightgray";
-
-    for (let i = 0; i < di.length; i++)
-    {
-        di[i].style.display = "block";
-    }
-
-    for (let i = 0; i < ve.length; i++)
-    {
-        ve[i].style.display = "none";
-    }
-
-    for (let i = 0; i <= urls.length; i++) {
-        audio[i].pause();
-        audio[i].currentTime = 0;
-    }
-}
-
-function selectreturn() {
-    but1.style.backgroundColor = "lightgray";
-    but2.style.backgroundColor = "darkgray";
-    
-    for (let i = 0; i < di.length; i++)
-    {
-        di[i].style.display = "none";
-    }
-
-    for (let i = 0; i < ve.length; i++)
-    {
-        ve[i].style.display = "block";
-    }
-
-    for (let i = 0; i <= urls.length; i++) {
-        audio[i].pause();
-        audio[i].currentTime = 0;
-    }
-}
+document.addEventListener('DOMContentLoaded', allFunction);
